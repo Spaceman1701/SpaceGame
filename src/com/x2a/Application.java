91d18b127189d0392e13b3c56492aa3cfb185700
Ethan Hunter <fc.spaceman@gmail.com>
2014-12-28 20:50:53 -0700

    Add window and draw panel. Can draw things. 0,0 is at the center of the window.

--- src/com/x2a/Application.java
+++ src/com/x2a/Application.java
@@ -5,7 +5,19 @@ package com.x2a;
  */
 public class Application {
 
+    private static final int X_RES = 768;
+    private static final int Y_RES = 768;
+
+    private static final int REFRESH_RATE = 60;
+
     public static void main(String[] args) {
-        
+        Application app = new Application();
+        app.run();
+    }
+
+    public void run() {
+        Window window = new Window(X_RES, Y_RES, REFRESH_RATE);
+
+        window.run();
     }
 }
---
+++ src/com/x2a/DrawPanel.java
@@ -0,0 +1,35 @@
+package com.x2a;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Ethan on 12/28/2014.
+ */
+public class DrawPanel extends JPanel{
+
+    private int xRes;
+    private int yRes;
+
+    public DrawPanel(int xRes, int yRes) {
+        this.xRes = xRes;
+        this.yRes = yRes;
+
+        setFocusable(true);
+        requestFocusInWindow();
+        setPreferredSize(new Dimension(xRes, yRes));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.clearRect(0, 0, getWidth(), getHeight());
+
+        g2.translate(getWidth()/2, getHeight()/2);
+
+        g2.setColor(Color.BLUE);
+        g2.fillRect(-getWidth()/4,-getHeight()/4, getWidth()/2, getHeight()/2);
+
+
+    }
+}
