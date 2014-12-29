db22e8f710c0c950db2e6b3fa5419adb18d7b563
David Gronlund <dagronlund@gmail.com>
2014-12-28 22:16:43 -0700

    Add node class

--- src/com/x2a/math/sat/ConvexHull.java
+++ src/com/x2a/math/sat/ConvexHull.java
@@ -1,10 +1,23 @@
 package com.x2a.math.sat;
 
+import com.x2a.math.Vector2;
+
+import java.util.List;
+
 /**
  * Created by David on 12/28/2014.
  */
 public class ConvexHull {
 
+    public class ConvexHullException extends Exception {
 
+    }
 
+    private ConvexHull(List<Vector2> outerPoints) {
+
+    }
+
+    public static ConvexHull createConvexHull(List<Vector2> points) {
+        return null;
+    }
 }
---
+++ src/com/x2a/scene/Node.java
@@ -0,0 +1,30 @@
+package com.x2a.scene;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by David on 12/28/2014.
+ */
+public class Node {
+
+    private Node parent;
+    private List<Node> children;
+
+    public Node() {
+        this(null);
+    }
+
+    public Node(Node parent) {
+        this.parent = parent;
+        children = new ArrayList<Node>();
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public void render() {
+        children.forEach((Node child) -> child.render());
+    }
+}
